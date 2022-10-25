5ac011db31e44f170b69875dc45130130f320bc1
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-10-25 18:23:01 +0300

    Completed task

--- rack/app.rb
+++ rack/app.rb
@@ -1,19 +1,44 @@
+require_relative 'format_parser'
+
 class App
   def call(env)
+    @request = Rack::Request.new(env)
+
+    response_handler
     [status, headers, body]
   end
 
   private
 
-  def status
-    200
+  def response_handler
+    return invalid_response if @request.path_info != '/time' || @request.query_string.empty?
+
+    @parser = FormatParser.new(@request)
+    @parser.format_valid? ? valid_format_response : invalid_format_response
   end
+
+  attr_reader :status
 
   def headers
     { "Content-Type" => "text/plain" }
   end
 
   def body
-    ["Yay, your first web application! <3"]
+    [@content]
+  end
+
+  def valid_format_response
+    @status = 200
+    @content = @parser.parsed_formats
+  end
+
+  def invalid_format_response
+    @status = 400
+    @content = "Unknown format: #{@parser.errors}"
+  end
+
+  def invalid_response
+    @status = 404
+    @content = "Not found."
   end
 end
---
+++ rack/format_parser.rb
@@ -0,0 +1,27 @@
+class FormatParser
+  ALLOWED_TIME_METHODS = %w[year hour minute day second month]
+
+  attr_reader :errors
+
+  def initialize(params)
+    @parsed_query_string = params['format'].split(',')
+    @errors = @parsed_query_string - ALLOWED_TIME_METHODS
+  end
+
+  def parsed_formats
+    @parsed_formats ||= parse_formats(@parsed_query_string)
+  end
+
+  def format_valid?
+    @errors.empty?
+  end
+
+  private
+
+  def parse_formats(format)
+    parsed_formats = format.map do |t|
+      Time.now.public_send(t.to_sym) unless @errors.include?(t)
+    end
+    parsed_formats.join('-')
+  end
+end
